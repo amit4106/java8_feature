633b2ac22d03af47007ed928da6afe0851722a73
amit4106 <amitkumarbharti4106@gmail.com>
2019-05-05 12:11:22 +0530

    FLOW CONTROL

---
+++ java_8/src/com/amit/java8/fundamental/FlowControl.java
@@ -0,0 +1,28 @@
+package com.amit.java8.fundamental;
+
+public class FlowControl {
+
+	public static void main(String[] args) 
+	{
+		int a=0;
+//switch(a) { 
+//case 21-10 : System.out.println("hello");
+//case 10 : System.out.println("not good");
+//default : System.out.println("default");
+//break;
+//case 30-18 : System.out.println("okey");//every case should be unique otherwise it will be show error.
+//}
+	
+//	while(a<5) 
+//	{
+//		a++;
+//		System.out.println("@mit");
+//	}
+		for(int i=1; i<=10; i++)
+			System.out.println(7*i);
+	
+	}
+
+	}
+
+
