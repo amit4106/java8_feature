0cb433db37ac32ff1f1498bc2c51419a74de4e8a
amit4106 <amitkumarbharti4106@gmail.com>
2021-07-26 19:20:20 +0530

    update

---
+++ java_8/src/com/amit/java8/task/Test2.java
@@ -0,0 +1,11 @@
+package com.amit.java8.task;
+
+public class Test2 {
+
+	static int a, b=10, c=4;
+	
+	static int sum=b+c;
+	public static void main(String[] args) {
+		System.out.println(sum);
+	}
+}
