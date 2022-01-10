2cf8827845ac56488bc23ad1ca3187683d3e9373
amit4106 <amitkumarbharti4106@gmail.com>
2022-01-10 18:45:19 +0530

    update

--- java_8/src/com/amit/java8/test/Test1.java
+++ java_8/src/com/amit/java8/test/Test1.java
@@ -3,18 +3,8 @@ package com.amit.java8.test;
 import java.util.Scanner;
 
 public class Test1 {
-
-	public void m1() {System.out.println("HELLO");}
-	public int m1(int a) {
-		this.m1();
-		return a;}
-	public void m1(int a, int b) {}
-	
-	
-	public static void main(String[] args) {
-	Test1 t=new Test1();
-	t.m1(10, 20);
-	System.out.println(t.m1(20));
-	}
-	}
-
+static {
+	System.out.println("hello");
+	System.exit(0);
+}
+}
