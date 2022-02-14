6abb1048f0cf4f6575f71dce8f5e5bca1d19b9dd
amit4106 <amitkumarbharti4106@gmail.com>
2022-02-14 14:53:20 +0530

    update

--- java_8/src/com/amit/java8/test/Test12.java
+++ java_8/src/com/amit/java8/test/Test12.java
@@ -5,15 +5,17 @@ import java.io.IOException;
 public class Test12 {
 
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder("Amit");
-		//sb.delete(2, 5);
-		sb.append("Kumar");
-		System.out.println(sb);
-//sb.append(new char[] {'K','U','M'}, 0,3);
-//System.out.println(sb);
+		//int i=3,k;
+		loop1:for (int i = 5; i <= 5; i--) {
+			System.out.print("");
+			if(i==0) break loop1;
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
-
 /*
- * ctrl + shift R
+ * ctrl + shift R to rename the method name.
  */
