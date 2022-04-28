e1c49ebd96327c6be0d18825541cea32be427bcd
amit4106 <amitkumarbharti4106@gmail.com>
2022-04-28 16:52:45 +0530

    updated by Amit dt 28/04/2022

--- practicQA/src/QA/Q2.java
+++ practicQA/src/QA/Q2.java
@@ -1,28 +1,35 @@
+/**
+ * Question: Find the Odd and Even number from the given int array -int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+ */
+
 package QA;
 
 public class Q2 {
 	static int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 	public static void evenNumber() {
-		for(int i =0; i<=arr.length-1;i++) {
-			if(i % 2 ==1) {
-				System.out.println(arr[i]);
+		System.out.print("Even numbers of int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } is : ");
+		for (int i = 0; i <= arr.length - 1; i++) {
+			if (i % 2 == 1) {
+				System.out.print(arr[i]+",");
 			}
 		}
+		System.out.println();
 	}
+
 	public static void oddNumber() {
-		for(int i =0; i<=arr.length-1;i++) {
-			if(i % 2 ==0) {
-				System.out.println(arr[i]);
+		System.out.print("Odd numbers of int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } is : ");
+		for (int i = 0; i <= arr.length - 1; i++) {
+			if (i % 2 == 0) {
+				System.out.print(arr[i]+",");
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		evenNumber();
 		oddNumber();
 
 	}
 }
-
