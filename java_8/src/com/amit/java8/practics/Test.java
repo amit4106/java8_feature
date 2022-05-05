a88061976082b6bda338adf46fa7d93ad8933126
amit4106 <amitkumarbharti4106@gmail.com>
2022-05-05 12:07:53 +0530

    by Amit dt 05/05/2022

--- java_8/src/com/amit/java8/practics/Test.java
+++ java_8/src/com/amit/java8/practics/Test.java
@@ -1,15 +1,5 @@
 package com.amit.java8.practics;
 
-import java.time.LocalDate;
-import java.time.MonthDay;
-import java.time.Period;
-import java.time.Year;
-
 public class Test {
-	public static void main(String[] args){
-		Year y = Year.of(2014);
-		LocalDate atMonthDay = y.atMonthDay(MonthDay.of(3	, 3));
-		System.out.println(atMonthDay);
-	}
-	
+
 }
--- practicQA/src/QA/Q1.java
+++ practicQA/src/QA/Q1.java
@@ -31,6 +31,7 @@ public class Q1 {
 	}
 
 	public static void main(String[] args) {
+		
 //		add();
 		dynamicAddition();
 	}
